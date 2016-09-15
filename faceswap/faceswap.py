de3286a4d0ee45c80b6a7c0688cbf0caab51d9c1
seith miller <semiller@lexmark.com>
2016-09-15 16:46:01 -0400

    add faceswap

--- faceswap/faceswap.py
+++ faceswap/faceswap.py
@@ -49,7 +49,7 @@ import numpy
 
 import sys
 
-PREDICTOR_PATH = "/home/matt/dlib-18.16/shape_predictor_68_face_landmarks.dat"
+PREDICTOR_PATH = "./shape_predictor_68_face_landmarks.dat"
 SCALE_FACTOR = 1 
 FEATHER_AMOUNT = 11
 
