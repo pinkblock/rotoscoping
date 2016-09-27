1c7d35db0670ff312ad12e82574c5a152295e398
seith miller <seithmiller@gmail.com>
2016-09-26 23:37:46 -0400

    fix apply_style.sh

--- execution/apply_style.sh
+++ execution/apply_style.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 #apply style
-NEURAL_STYLE_PATH=./implementation/neural_artistic_style
-python $NEURAL_STYLE_PATH/style_network.py --subject ./output_images/face1.jpg --style $NEURAL_STYLE_PATH/images/starry_night.jpg  ./output_images/demo_thing.jpg
-
+NEURAL_STYLE_PATH=implementation/neural_artistic_style/
+MY_SUB=../../input_images/ns_content_image/golden_gate.jpg
+MY_STYLE=../../input_images/ns_style_image/skrik.jpg
+MY_OUT=../../output_images/demo_thing.jpg
+cd $NEURAL_STYLE_PATH
+python neural_artistic_style.py --subject $MY_SUB --style $MY_STYLE --output $MY_OUT
