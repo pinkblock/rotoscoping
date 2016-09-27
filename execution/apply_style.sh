e73e1131674d9b1dec5fb0d8e451cae7009dd13c
seith miller <andrew.lordasm@gmail.com>
2016-09-27 15:15:18 -0400

    update apply_style.sh (#2)

--- execution/apply_style.sh
+++ execution/apply_style.sh
@@ -5,5 +5,6 @@ NEURAL_STYLE_PATH=implementation/neural_artistic_style/
 MY_SUB=../../input_images/ns_content_image/golden_gate.jpg
 MY_STYLE=../../input_images/ns_style_image/skrik.jpg
 MY_OUT=../../output_images/demo_thing.jpg
+MY_I=1
 cd $NEURAL_STYLE_PATH
-python neural_artistic_style.py --subject $MY_SUB --style $MY_STYLE --output $MY_OUT
+python neural_artistic_style.py --subject $MY_SUB --style $MY_STYLE --output $MY_OUT --iterations $MY_I
