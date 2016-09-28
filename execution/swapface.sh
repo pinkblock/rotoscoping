f5406439a21eb68e4b61e5927112e6bb88c6a52a
seith miller <you@example.com>
2016-09-28 01:51:44 -0400

    update swapface

--- execution/swapface.sh
+++ execution/swapface.sh
@@ -2,5 +2,7 @@
 
 #Swap a face
 FACE_REPLACE_PATH=./implementation/face-replace
-python $FACE_REPLACE_PATH/change-face-in-video.py $FACE_REPLACE_PATH/demo/arni.jpg $FACE_REPLACE_PATH/demo/Barack_Obama.jpg ./output_images/demo_arni_Obama.mov
-
+python2 $FACE_REPLACE_PATH/change-face-in-video.py \
+    $FACE_REPLACE_PATH/demo/arni.jpg \
+    $FACE_REPLACE_PATH/demo/Barack_Obama.jpg \
+    ./output_images/demo_arni_Obama.mov
--- execution/swapface2.sh
+++ execution/swapface2.sh
@@ -2,5 +2,10 @@
 
 #faceswap ./faceswap.py <head image> <face image>
 FACE_REPLACE_PATH=./implementation/faceswap
-python $FACE_REPLACE_PATH/faceswap.py ./input_images/fs_target_faces/margrethe.jpg ./input_images/fs_masks/face1.jpg ./output_images/swap_for_style.mov
+
+cd $FACE_REPLACE_PATH
+python2 faceswap.py \
+    face1.jpg \
+    face2.jpg \
+    output.jpg
 
