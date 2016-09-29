c966917b9dfd2fb681750e53fb3bd9aae4e2a19c
seith miller <seithmiller@gmail.com>
2016-09-28 20:31:56 -0400

    update apply_style.sh

--- execution/apply_style.sh
+++ execution/apply_style.sh
@@ -7,4 +7,8 @@ MY_STYLE=../../input_images/ns_style_image/skrik.jpg
 MY_OUT=../../output_images/demo_thing.jpg
 MY_I=1
 cd $NEURAL_STYLE_PATH
-python neural_artistic_style.py --subject $MY_SUB --style $MY_STYLE --output $MY_OUT --iterations $MY_I
+python neural_artistic_style.py \
+    --subject $MY_SUB \
+    --style $MY_STYLE \
+    --output $MY_OUT \
+    --iterations $MY_I
