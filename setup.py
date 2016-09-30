60bc275a7c45278ffe9269abf2c1191a73df9d40
seith miller <andrew.lordasm@gmail.com>
2016-09-29 21:01:06 -0400

    add setup.py (#6)

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rotoscoping',
+      version='1.0',
+      description="Take in pics swap faces and make'm pretty",
+      author='Seith Miller',
+      author_email='seithmiller@gmail.com',
+      url='https://www.seithmiller.com/',
+      packages=['rotoscoping'],
+     )
